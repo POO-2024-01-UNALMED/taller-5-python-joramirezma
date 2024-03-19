e8aa9e00a1c623bb43ebdf22883ff72af6de87b6
Johan Ramirez <159493340+joramirezma@users.noreply.github.com>
2024-03-18 21:08:19 -0500

    a

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,8 +1,3 @@
-from zooAnimales.ave import Ave
-from zooAnimales.anfibio import Anfibio
-from zooAnimales.mamifero import Mamifero
-from zooAnimales.pez import Pez
-from zooAnimales.reptil import Reptil
 
 class Animal():
     _totalAnimales=0
@@ -60,4 +55,10 @@ class Animal():
         
     @staticmethod
     def totalPorTipo():
+        from zooAnimales.ave import Ave
+        from zooAnimales.anfibio import Anfibio
+        from zooAnimales.mamifero import Mamifero
+        from zooAnimales.pez import Pez
+        from zooAnimales.reptil import Reptil
+
         return "Mamiferos: " + str(Mamifero.cantidadMamiferos()) + "\nAves: " + str(Ave.cantidadAves()) + "\nReptiles: " + str(Reptil.cantidadReptiles()) + "\nPeces: " + str(Pez.cantidadPeces()) + "\nAnfibios: " + str(Anfibio.cantidadAnfibios())
